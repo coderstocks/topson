7aceaa172578866411b5cb0f3168751783bf99ff
Viral Ads Media <info.viraladsmedia@gmail.com>
2025-01-24 16:29:35 +0530

    FSDFSD

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,47 @@ $('.review').slick({
   slidesToShow: 1,
   slidesToScroll: 1
 });
-
+// slick
+$('.people').slick({
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 200,
+  autoplay: true,
+  prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="bi bi-arrow-left"></i></button>',
+  nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="bi bi-arrow-right"></i></button>',
+  autoplaySpeed: 2000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 // JavaScript for Cursor Effect
 const cursor = document.querySelector(".custom-cursor");
@@ -43,3 +83,23 @@ function slideImage(){
 }
 
 window.addEventListener('resize', slideImage);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.querySelector(".brand-carousel");
+  const items = Array.from(carousel.children);
+
+  // Calculate the total width of the items
+  let totalWidth = 0;
+  items.forEach((item) => {
+    totalWidth += item.offsetWidth + 50; // Including margin
+  });
+
+  // Set the width of the carousel to the total width
+  carousel.style.width = `${totalWidth}px`;
+
+  // Duplicate items to create a seamless scroll effect
+  items.forEach((item) => {
+    const clone = item.cloneNode(true);
+    carousel.appendChild(clone);
+  });
+});
