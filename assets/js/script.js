aa76861649ea4afaab3fe33628ccc0599aba8a97
Viral Ads Media <info.viraladsmedia@gmail.com>
2025-02-06 18:04:12 +0530

    FSDFSDF

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,46 @@ $('.people').slick({
     // instead of a settings object
   ]
 });
+$('.people2').slick({
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 200,
+  autoplay: true,
+  prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="bi bi-arrow-left"></i></button>',
+  nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="bi bi-arrow-right"></i></button>',
+  autoplaySpeed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 // slick
 $('.reels').slick({
   dots: false,
